81be9ff6809656cd832ebded06a17bbce5940c0b
Rivan Henrique Bello <rivanbello@gmail.com>
2020-04-16 14:08:17 -0300

    Criacao projeto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const { uuid, isUuid } = require('uuidv4');
+
+const app = express();
+
+//Qualquer front terá acesso esse back end
+app.use(cors());
+
+//Necessário para usar na requisições post o recurso JSON
+app.use(express.json());
+
+const projects =[];
+
+app.get('/notificarVenda', (request, response) => {
+  console.log(request);
+});
+
+app.listen(3333, () => {
+  console.log('Back-end started!');
+});
