a11d1e197b9db3493a1f666ead199e4426f802a8
Rivan Henrique Bello <rivanbello@gmail.com>
2020-04-16 15:07:14 -0300

    Testar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+var fs = require('fs');
 
 const app = express();
 
 //Necessário para usar na requisições post o recurso JSON
 app.use(express.json());
 
-const projects =[];
-
 app.get('/venda', (request, response) => {
-  return response.json({message: "Teste executado!"})
+  const { query } = request;
+  fs.writeFile("./src/vendas.txt", `${JSON.stringify(query)},` ,{enconding:'utf-8',flag: 'a+'}, function(erro) {
+      
+    if(erro) {
+          throw erro;
+      }
+
+      return response.json({message: "Venda regristada com suscesso!"})
+  });  
+});
+
+app.get('/listar-vendas', (request, response) => {
+  fs.readFile('./src/vendas.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    return response.json(data);
+  });
 });
 
 app.listen(process.env.PORT || 3333, () => {
